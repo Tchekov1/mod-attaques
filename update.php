d7f877b72610ab1f10db79d73d4ffe78d3fbd029
darknoon29 <darknoon@darkcity.fr>
2020-01-16 14:02:39 +0100

    Version 2.0.0
    Upgrade Done

--- update.php
+++ update.php
@@ -14,6 +14,10 @@ if (!defined('IN_SPYOGAME')) {
 
 global $db,$table_prefix;
 
+//On récupère la version actuelle du mod
+$query = "SELECT id, version FROM ".TABLE_MOD." WHERE action='attaques'";
+$result = $db->sql_query($query);
+list($mod_id, $version) = $db->sql_fetch_row($result);
 
 $query = "CREATE TABLE IF NOT EXISTS `" . $table_prefix . "mod_user_config` (
             `mod` VARCHAR(50) NOT NULL,
@@ -26,6 +30,17 @@ $query = "CREATE TABLE IF NOT EXISTS `" . $table_prefix . "mod_user_config` (
         DEFAULT CHARSET = utf8";
 
 $db->sql_query($query);
+
+if (version_compare( $version , '2.0.0.', '<')) {
+
+    // on insère les valeurs de configuration par défaut car changement de format de données
+    $sqldata = '{"transp":75,"layer":1,"defenseur":1,"histo":1}';
+    mod_set_option('config', $sqldata);
+
+// on insère les valeurs bbcodes par défaut
+    $sqldata = '{"title":"#FFA500","m_g":"#00FF40","c_g":"#00FF40","d_g":"#00FF40","m_r":"#00FF40","c_r":"#00FF40","perte":"#FF0000","renta":"#00FF40"}';
+    mod_set_option('bbcodes', $sqldata);
+}
 
 
 // Puis on change le numéro de version
