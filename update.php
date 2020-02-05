f96507c0ed31c045f9c888b66825cb2c7d1d64f5
darknoon29 <darknoon@darkcity.fr>
2020-02-05 13:54:36 +0100

    Escape " in JSON Arrays

--- update.php
+++ update.php
@@ -34,11 +34,11 @@ $db->sql_query($query);
 if (version_compare( $version , '2.0.0.', '<')) {
 
     // on insère les valeurs de configuration par défaut car changement de format de données
-    $sqldata = '{"transp":75,"layer":1,"defenseur":1,"histo":1}';
+    $sqldata = '{\"transp\":75,\"layer\":1,\"defenseur\":1,\"histo\":1}';
     mod_set_option('config', $sqldata);
 
 // on insère les valeurs bbcodes par défaut
-    $sqldata = '{"title":"#FFA500","m_g":"#00FF40","c_g":"#00FF40","d_g":"#00FF40","m_r":"#00FF40","c_r":"#00FF40","perte":"#FF0000","renta":"#00FF40"}';
+    $sqldata = '{\"title\":\"#FFA500\",\"m_g\":\"#00FF40\",\"c_g\":\"#00FF40\",\"d_g\":\"#00FF40\",\"m_r\":\"#00FF40\",\"c_r\":\"#00FF40\",\"perte\":\"#FF0000\",\"renta\":\"#00FF40\"}';
     mod_set_option('bbcodes', $sqldata);
 }
 
